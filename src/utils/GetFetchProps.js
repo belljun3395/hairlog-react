3b5228634ba60494bdb0868dd8163167953f8a20
belljun3395 <195850@jnu.ac.kr>
2023-05-17 10:17:31 +0900

    feat(utils): GetFetchProps 구현

---
+++ src/utils/GetFetchProps.js
@@ -0,0 +1,68 @@
+import {
+  baseRecordBodyConverter,
+  categoryRecordBodyConverter,
+} from "./Converter";
+const JSON_HEADER = { "Content-Type": "application/json" };
+
+
+export function getAuthHeader(authCookie) {
+  return {
+    "Content-Type": "application/json",
+    Authorization: "Bearer " + authCookie,
+  };
+}
+
+export function getSignUpFetchProps(data) {
+  return {
+    method: "POST",
+    headers: JSON_HEADER,
+    body: JSON.stringify({
+      email: data.email,
+      password: data.password,
+      name: data.name,
+      sex: data.sex,
+    }),
+  };
+}
+
+export function getLogInFetchProps(data) {
+  return {
+    method: "POST",
+    headers: JSON_HEADER,
+    body: JSON.stringify({
+      email: data.id,
+      password: data.password,
+    }),
+  };
+}
+
+export function getDesignerFetchProps(authCookie, data) {
+  return {
+    method: "POST",
+    headers: getAuthHeader(authCookie),
+    body: JSON.stringify({
+      designerName: data.designerName,
+      designerSalon: data.designerSalon,
+      designerFav: true,
+    }),
+  };
+}
+
+export function getRecordWriteFetchProps(
+  authCookie,
+  data,
+  category,
+  hurt,
+  date
+) {
+  return {
+    method: "POST",
+    headers: getAuthHeader(authCookie),
+    body: JSON.stringify(
+      Object.assign(
+        baseRecordBodyConverter(data, date),
+        categoryRecordBodyConverter(category, data, hurt)
+      )
+    ),
+  };
+}
