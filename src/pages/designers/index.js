b3bcbdd74946de73740104159c4b5c6704f8f2e5
belljun3395 <195850@jnu.ac.kr>
2023-05-17 10:54:21 +0900

    feat(pages): designers 구현

---
+++ src/pages/designers/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import HairlogHead from "../../component/common/head/HairLogHead";
+import DesignersList from "../../component/designers/DesignersList";
+import Navbar from "../../component/common/navbar/Navbar";
+
+function Desingers() {
+  return (
+    <div>
+      <HairlogHead />
+      <DesignersList />
+      <Navbar />
+    </div>
+  );
+}
+
+export default Desingers;
