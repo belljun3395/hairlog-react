f22f35a756e36364bbc0aa9e3db249eedfe1ff73
belljun3395 <195850@jnu.ac.kr>
2023-05-17 11:07:39 +0900

    feat: App 라우팅 구현

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import Login from "./pages/login";
+import Register from "./pages/signUp";
+import Welcome from "./pages/welcome";
+import RecordWrite from "./pages/record/write";
+import RecordRead from "./pages/record/read";
+import Designers from "./pages/designers";
+import MyPage from "./pages/myPage";
 
 function App() {
   return (
     <Routes>
-      <Route />
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signUp" element={<Register />} />
+      <Route path="/welcome" element={<Welcome />} />
+      <Route path="/record" element={<RecordWrite />} />
+      <Route path="/records" element={<RecordRead />} />
+      <Route path="/designers" element={<Designers />} />
+      <Route path="/mypage" element={<MyPage />} />
     </Routes>
   );
 }
