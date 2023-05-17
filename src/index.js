0d38923c13e36b6f604f3d7f8fc2c603fa0222d7
belljun3395 <195850@jnu.ac.kr>
2023-05-17 10:13:21 +0900

    feat: pwa 적용 설정

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -9,3 +11,7 @@ root.render(
     <App />
   </BrowserRouter>
 );
+
+serviceWorkerRegistration.register();
+
+reportWebVitals();
