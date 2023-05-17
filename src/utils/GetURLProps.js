5432210916058ad3a2d7d93351011299dae4c04e
belljun3395 <195850@jnu.ac.kr>
2023-05-17 10:20:06 +0900

    feat(utils): GetURLProps 구현

---
+++ src/utils/GetURLProps.js
@@ -0,0 +1,30 @@
+const BASE_URL = "http://localhost:8080"
+const PAGESIZE = 6;
+
+export const GET_DESIGNER_URL = BASE_URL+"/api/v1/designers";
+export const POST_LOGIN_URL = BASE_URL+"/api/v1/members/login";
+export const GET_DESIGNERLIST_URL = BASE_URL+"/api/v1/designers";
+export const POST_SIGNUP_URL = BASE_URL+"/api/v1/members";
+export const GET_MEMBERS_URL = BASE_URL+"/api/v1/members";
+
+export function getRecordsPagesURL(category, page) {
+  if (category === undefined) {
+    return BASE_URL+`/api/v1/records/pages?page=${page}&size=${PAGESIZE}`;
+  } else {
+    return BASE_URL+`/api/v1/records/pages?page=${page}&size=${PAGESIZE}&c=${category}`;
+  }
+}
+
+export function getRecordURL(recordId, category) {
+  return BASE_URL+`/api/v1/records?rid=${recordId}&c=${category}`;
+}
+
+export function getRecordWriteUrl(category) {
+  if (category === "CUT") {
+    return BASE_URL+"/api/v1/records/cut";
+  } else if (category === "PERM") {
+    return BASE_URL+"/api/v1/records/perm";
+  } else if (category === "DYEING") {
+    return BASE_URL+"/api/v1/records/dyeing";
+  }
+}
